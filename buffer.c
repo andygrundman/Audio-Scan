56b6b46587eb2912a42578c524b5f57180bf6eac
Andy Grundman <andy@hybridized.org>
2009-04-03 18:54:13 +0000

    u_int32_t -> uint32_t for win32 support

--- buffer.c
+++ buffer.c
@@ -266,16 +266,16 @@ buffer_get_char(Buffer *buffer)
 	return (u_char) ch;
 }
 
-u_int32_t
+uint32_t
 get_u32le(const void *vp)
 {
   const u_char *p = (const u_char *)vp;
-  u_int32_t v;
+  uint32_t v;
 
-  v  = (u_int32_t)p[3] << 24;
-  v |= (u_int32_t)p[2] << 16;
-  v |= (u_int32_t)p[1] << 8;
-  v |= (u_int32_t)p[0];
+  v  = (uint32_t)p[3] << 24;
+  v |= (uint32_t)p[2] << 16;
+  v |= (uint32_t)p[1] << 8;
+  v |= (uint32_t)p[0];
 
   return (v);
 }
