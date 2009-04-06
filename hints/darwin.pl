744a7f4a6b9630863660e4b6ff6fcaac7454da7f
Andy Grundman <andy@hybridized.org>
2009-04-06 13:43:19 +0000

    Don't build universal binary on PPC Macs

--- hints/darwin.pl
+++ hints/darwin.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 
-$arch = "-arch i386 -arch ppc";
-print "Adding $archn";
+if ( $Config{myarchname} =~ /i386/ ) {
+	$arch = "-arch i386 -arch ppc";
+	print "Adding $archn";
 
-$self->{CCFLAGS} = "$arch $Config{ccflags}";
-$self->{LDFLAGS} = "$arch $Config{ldflags}";
-$self->{LDDLFLAGS} = "$arch $Config{lddlflags}";
+	$self->{CCFLAGS} = "$arch $Config{ccflags}";
+	$self->{LDFLAGS} = "$arch $Config{ldflags}";
+	$self->{LDDLFLAGS} = "$arch $Config{lddlflags}";
+}
