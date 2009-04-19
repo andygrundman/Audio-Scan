40c5b4133d5e6c2621de49f969d7f44eb03e318d
Andy Grundman <andy@hybridized.org>
2009-04-19 20:39:44 +0000

    Clean up buffer.h prototypes

--- include/buffer.h
+++ include/buffer.h
@@ -27,33 +27,41 @@
 #endif
 
 typedef struct {
-	u_char	*buf;		/* Buffer for data. */
-	u_int	 alloc;		/* Number of bytes allocated for data. */
-	u_int	 offset;	/* Offset of first byte containing data. */
-	u_int	 end;		/* Offset of last byte containing data. */
+  u_char  *buf;   /* Buffer for data. */
+  u_int  alloc;   /* Number of bytes allocated for data. */
+  u_int  offset;  /* Offset of first byte containing data. */
+  u_int  end;     /* Offset of last byte containing data. */
 } Buffer;
 
-void buffer_init(Buffer *, uint32_t);
-void buffer_clear(Buffer *);
-void buffer_free(Buffer *);
+void buffer_init(Buffer *buffer, uint32_t len);
+void buffer_free(Buffer *buffer);
+void buffer_clear(Buffer *buffer);
+void buffer_append(Buffer *buffer, const void *data, uint32_t len);
+static int buffer_compact(Buffer *buffer);
+void * buffer_append_space(Buffer *buffer, uint32_t len);
+int buffer_check_alloc(Buffer *buffer, uint32_t len);
+uint32_t buffer_len(Buffer *buffer);
+int buffer_get_ret(Buffer *buffer, void *buf, uint32_t len);
+void buffer_get(Buffer *buffer, void *buf, uint32_t len);
+int buffer_consume_ret(Buffer *buffer, uint32_t bytes);
+void buffer_consume(Buffer *buffer, uint32_t bytes);
+int buffer_consume_end_ret(Buffer *buffer, uint32_t bytes);
+void buffer_consume_end(Buffer *buffer, uint32_t bytes);
+void * buffer_ptr(Buffer *buffer);
+void buffer_dump(Buffer *buffer, uint32_t len);
+int buffer_get_char_ret(char *ret, Buffer *buffer);
+int buffer_get_char(Buffer *buffer);
+uint32_t get_u32le(const void *vp);
+int buffer_get_int_le_ret(uint32_t *ret, Buffer *buffer);
+uint32_t buffer_get_int_le(Buffer *buffer);
+uint64_t get_u64le(const void *vp);
+int buffer_get_int64_le_ret(uint64_t *ret, Buffer *buffer);
+uint64_t buffer_get_int64_le(Buffer *buffer);
+uint16_t get_u16le(const void *vp);
+int buffer_get_short_le_ret(uint16_t *ret, Buffer *buffer);
+uint16_t buffer_get_short_le(Buffer *buffer);
+void buffer_put_char(Buffer *buffer, int value);
+void buffer_get_utf16le_as_utf8(Buffer *buffer, Buffer *utf8, uint32_t len);
+void buffer_get_guid(Buffer *buffer, GUID *g);
 
-u_int	buffer_len(Buffer *);
-void *buffer_ptr(Buffer *);
-
-void buffer_append(Buffer *, const void *, u_int);
-void *buffer_append_space(Buffer *, u_int);
-
-int	buffer_check_alloc(Buffer *, u_int);
-
-void buffer_get(Buffer *, void *, u_int);
-
-void buffer_consume(Buffer *, u_int);
-void buffer_consume_end(Buffer *, u_int);
-
-void buffer_dump(Buffer *, uint32_t);
-
-int	buffer_get_ret(Buffer *, void *, u_int);
-int	buffer_consume_ret(Buffer *, u_int);
-int	buffer_consume_end_ret(Buffer *, u_int);
-
-#endif				/* BUFFER_H */
+#endif
