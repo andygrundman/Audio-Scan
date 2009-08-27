1dc6ff6ac037e0e902022516be9bf655516c0502
Andy Grundman <andy@hybridized.org>
2009-08-27 01:34:31 +0000

    Update hints file to build 64-bit binaries for Snow Leopard

--- hints/darwin.pl
+++ hints/darwin.pl
@@ -1,10 +1,20 @@
 #!/usr/bin/perl
 
-if ( $Config{myarchname} =~ /i386/ ) {
-	$arch = "-arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3";
-	print "Adding $archn";
+use Config;
 
-	$self->{CCFLAGS} = "$arch $Config{ccflags}";
-	$self->{LDFLAGS} = "$arch -L/usr/lib $Config{ldflags}";
-	$self->{LDDLFLAGS} = "$arch $Config{lddlflags}";
+if ( $Config{myarchname} =~ /i386/ ) {
+    if ( $Config{version} =~ /^5\.10/ ) {
+        # 5.10, build as 10.4+ with Snow Leopard 64-bit support
+        $arch = "-arch i386 -arch x86_64 -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4";
+    }
+    else {
+        # 5.8.x, build for 10.3+ 32-bit universal
+        $arch = "-arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3";
+    }
+    
+    print "Adding $arch";
+
+    $self->{CCFLAGS} = "$arch $Config{ccflags}";
+    $self->{LDFLAGS} = "$arch -L/usr/lib $Config{ldflags}";
+    $self->{LDDLFLAGS} = "$arch $Config{lddlflags}";
 }
