9e20566167a4f4900367a41132e46adc34923cef
Andy Grundman <andy@hybridized.org>
2010-03-23 18:22:58 +0000

    Fix Mac hints to compile faster for dev

--- hints/darwin.pl
+++ hints/darwin.pl
@@ -9,8 +9,10 @@ if ( $Config{myarchname} =~ /i386/ ) {
     if ( my @archs = $Config{ccflags} =~ /-arch ([^ ]+)/g ) {
         $arch = join( '', map { "-arch $_ " } @archs );
         
-        # XXX for development, you can use only one arch to speed up compiles
-        $arch = '-arch x86_64 ';
+        if ( -e 'MANIFEST.SKIP' ) {
+            # XXX for development, use only one arch to speed up compiles
+            $arch = '-arch x86_64 ';
+        }
     }
     
     # Read OS version
